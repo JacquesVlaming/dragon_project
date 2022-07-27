d497fbece2d47a5d6afcc7e10d0449ce99e1fc11
Jacques Vlaming <jacques.vlaming@gmail.com>
2022-07-27 19:41:53 +0200

    dev

--- maintain_temp.py
+++ maintain_temp.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT
 import requests
 import RPi.GPIO as GPIO
-import time
+# import time
 
 
 def relay_state(raw_state):
@@ -34,7 +34,7 @@ sensor_type = 22
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor_type, pin)
 
-print('Current temp: ' + str(temperature))
+# print('Current temp: ' + str(temperature))
 
 GPIO.setmode(GPIO.BCM)
 
@@ -42,15 +42,15 @@ GPIO.setmode(GPIO.BCM)
 
 if temperature < lower_limit:
     set_state(20, 'high')
-    print('Heater on')
+    # print('Heater on')
     current_state = 1
 
 if upper_limit > temperature > lower_limit:
     set_state(20, 'low')
-    print('Heater off')
+    # print('Heater off')
     current_state = 0
 
 r = requests.post('https://api.thingspeak.com/update.json', data={'api_key': thingspeak_key, 'field1': temperature,
                                                                   'field2': humidity, 'field3': current_state})
 
-time.sleep(60)
+# time.sleep(60)
