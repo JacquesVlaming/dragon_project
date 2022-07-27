e63e3e137e7d75d3480f0b8236cf4a6e7d481378
Jacques Vlaming <jacques.vlaming@gmail.com>
2022-07-27 20:09:15 +0200

    dev

--- maintain_temp.py
+++ maintain_temp.py
@@ -4,13 +4,6 @@ import RPi.GPIO as GPIO
 # import time
 
 
-def relay_state(raw_state):
-    if raw_state == 0:
-        return 1
-    else:
-        return 0
-
-
 def set_state(set_pin, new_state):
     if new_state == 'high':
         GPIO.setup(set_pin, GPIO.LOW)
@@ -20,37 +13,34 @@ def set_state(set_pin, new_state):
         print('unknown state')
 
 
-upper_limit = 25
-
-lower_limit = 24
+ideal = 24
+upper_limit = ideal + 0.5
+lower_limit = ideal - 0.5
+pin = 4
+thingspeak_key = 'ENOI1RNJHYXDY80C'
+sensor_type = 22
+current_state = 0
 
 GPIO.setmode(GPIO.BCM)
 
-pin = 4
-
-thingspeak_key = 'ENOI1RNJHYXDY80C'
-
-sensor_type = 22
-
+# while True:
 humidity, temperature = Adafruit_DHT.read_retry(sensor_type, pin)
 
-# print('Current temp: ' + str(temperature))
+print('Current temp: ' + str(temperature))
 
 GPIO.setmode(GPIO.BCM)
 
-# current_state = 0
-
-if temperature < lower_limit:
+if upper_limit > temperature < lower_limit:
     set_state(20, 'high')
-    # print('Heater on')
+    print('Heater on')
     current_state = 1
 
-if upper_limit > temperature > lower_limit:
+if upper_limit < temperature > lower_limit:
     set_state(20, 'low')
-    # print('Heater off')
+    print('Heater off')
     current_state = 0
 
 r = requests.post('https://api.thingspeak.com/update.json', data={'api_key': thingspeak_key, 'field1': temperature,
                                                                   'field2': humidity, 'field3': current_state})
 
-# time.sleep(60)
+    # time.sleep(60)
