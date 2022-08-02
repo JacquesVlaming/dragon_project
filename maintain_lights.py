53745f7bb07d0739e2de53952ddf2d02e76317f1
jacques <jacques.vlaming@gmail.com>
2022-08-02 16:29:01 +0200

    commit

---
+++ maintain_lights.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+def set_state(set_pin, new_state):
+    if new_state == 'high':
+        GPIO.setup(set_pin, GPIO.LOW)
+    elif new_state == 'low':
+        GPIO.setup(set_pin, GPIO.HIGH)
+    else:
+        print('unknown state')
+
+set_state(21, 'high')
