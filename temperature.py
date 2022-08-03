636a25aeadedac8baf0f75c5eb8905652369dbc8
jacques <jacques.vlaming@gmail.com>
2022-08-03 11:40:03 +0200

    commit

--- temperature.py
+++ temperature.py
@@ -5,7 +5,7 @@ import time
 import datetime
 import csv
 
-times = [8, 9, 10]
+times = [8, 9, 10, 11]
 
 pin = 4
 thingspeak_key = 'ENOI1RNJHYXDY80C'
@@ -47,7 +47,7 @@ while True:
             GPIO.setup(21, GPIO.LOW)
             light_state = 1
         else:
-            GPIO.setup(20, GPIO.HIGH)
+            GPIO.setup(21, GPIO.HIGH)
             light_state = 0
 
         r = requests.post('https://api.thingspeak.com/update.json', data={'api_key': thingspeak_key,
